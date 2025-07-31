19d38b25abc6a22d3bd7a07830a806d86409035a
Diego Bonze <diegobonze747@gmail.com>
2025-07-31 11:52:21 -0300

    devolvendo os pgn como arquivo estático no URL

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import path from 'node:path';
+
 import { router } from './router';
 
   mongoose.connect('mongodb://localhost:27017/waiter-app')
@@ -7,6 +9,7 @@ import { router } from './router';
       const app = express()
       const port = 3001;
 
+      app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
       app.use(express.json())
       app.use(router)
 
