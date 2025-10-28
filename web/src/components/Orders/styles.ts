2ec7c119096791654aa0bc8ca16feb30ff269244
Diego Bonze <diegobonze747@gmail.com>
2025-10-28 17:31:56 -0300

    componentizando os Orders

--- web/src/components/Orders/styles.ts
+++ web/src/components/Orders/styles.ts
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 
-
 export const Container = styled.div`
   width: 100%;
   max-width: 1216px;
@@ -8,54 +7,3 @@ export const Container = styled.div`
   display: flex;
   gap: 32px;
 `;
-
-export const Board = styled.div`
-  padding: 16px;
-  border: 1px solid rgba(204, 204, 204, 0.4);
-  border-radius: 16px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  flex: 1;
-
-  header {
-    padding: 8px;
-    font-size: 14px;
-    display: flex;
-    align-items: center;
-    gap: 8px;
-  }
-`;
-
-export const OrdersContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-
-  button {
-    width: 100%;
-    margin-top: 24px;
-    background: #fff;
-    border: 1px solid rgba(204, 204, 204, 0.4);
-    border-radius: 8px;
-    height: 128px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    gap: 8px;
-
-    strong {
-      font-weight: 500;
-    }
-
-    span {
-      font-size: 14px;
-      color: #666;
-    }
-
-    & + button {
-      margin-top: 24px;
-    }
-  }
-`
---
+++ web/src/components/OrdersBoard/index.tsx
@@ -0,0 +1,24 @@
+import { OrdersContainer, Board } from "./styles";
+
+export function OrdersBoard(props) {
+  return (
+    <Board>
+      <header>
+        <span>🕑</span>
+        <strong>Fila de espera</strong>
+        <span>(1)</span>
+      </header>
+
+      <OrdersContainer>
+        <button type="button">
+          <strong>Mesa 2</strong>
+          <span>2 itens</span>
+        </button>
+        <button type="button">
+          <strong>Mesa 2</strong>
+          <span>2 itens</span>
+        </button>
+      </OrdersContainer>
+    </Board>
+  )
+}
